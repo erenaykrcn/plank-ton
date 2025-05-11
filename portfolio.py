6522631ca9387c0086f3c31bf7dbb4a84d0e83b1
Erenay Karacan <erenaykaracan@Erenays-MacBook-Pro.local>
2025-05-11 08:20:47 +0200

    migrate from notebook

--- portfolio.py
+++ portfolio.py
@@ -1,31 +1,262 @@
-from qiskit_aer.primitives import SamplerV2
+#from qiskit_aer.primitives import SamplerV2
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
 import pandas as pd
 import random
+import numpy as np
+
+from qiskit.quantum_info import state_fidelity
+from qiskit.quantum_info import random_statevector
+
+import scipy
+import qiskit
+from qiskit import Aer, execute, transpile
+
+import sys
+sys.path.append("./src/groundstate_prep")
+sys.path.append("./src/rqcopt")
+from ground_state_prep_qiskit import qetu_rqc_oneLayer
 
 
 
 def run(input_data):
+    d = input_data
+    
+    # We are taking the top 4 assets with highest 
+    # expected returns and will optimize over them
+    # This is of course a significantly low number
+    # but for simulability of small scale quantum 
+    # computers we limit ourselves to small data set and low qubit counts.
+    filtering_number = 4 # User input
 
-    # How to read input data:
-    num_assets=input_data['num_assets']
-    assets=input_data['assets']
-    evaluation_date=input_data['evaluation_date']
-
-    df=pd.DataFrame()
-    series=[]
-    for asset in assets:
-        series.append(pd.Series(assets[asset]['history'],name=asset))
-    df=pd.concat(series,axis=1)
+    L = d['num_assets']
+    asset_names = list(d["assets"].keys())
+    p = [list(d['assets'][name]['history'].values()) for name in asset_names] # Stock prices over time
+    P = [p[i][-1] if len(p[i])!=0 else 0 for i in range(L)] # Current Stock Prices
 
 
-    # How to return output data:
-    aux={}
-    for asset in assets:
-        if random.random()>0.95:
-            aux[asset]=random.randint(1,10000000)
-    result={}
-    for asset in aux:
-        result[asset]=aux[asset]/sum(aux.values())
+    # Expected Return:
+    r = [[(p[i][t]-p[i][t-1])/p[i][t-1] if t>0 else 0 for t in range(len(p[i]))] for i in range(len(p))]
+    mu = []
+    for i in range(L):
+        exp = np.sum(np.array([r[i][t] for t in range(len(r[i]))])) / len(r[i])
+        if not np.isnan(exp):
+            mu.append(exp)
+        else:
+            mu.append(-1)
+
+
+    # We take only the highest 10 expected returns and will optimize over them.
+    # This can be user defined parameter. As the quantum computer scales up,
+    # we'll be able to increase this number.
+    indices = np.argsort(mu)
+    mu_filtered = []
+    r_filtered = []
+    p_filtered = []
+    for index in indices[-filtering_number:]:
+        mu_filtered.append(mu[index])
+        r_filtered.append(r[index])
+        p_filtered.append(p[index])
+
+    mu = mu_filtered
+    r = r_filtered
+    L = filtering_number
+    p = p_filtered
+    P = [p[i][-1] for i in range(L)] # Current Stock Prices, after initial filtering.
+
+    # We have a fixed budget for the number of qubits that this algortihm is
+    # 'allowed' to use. The more qubits you can accommodate on your hardware,
+    # the higher of a 'filtering_number' you can choose and optimize over
+    # a larger set of assets. For our simulation reasons, we hard-code a very low
+    # number of qubits as budget - as we'll use state_vector simulations.
+    N = 8 # User input, algortihm will use N +- 1 many qubits.
+
+    N = N - 1
+    # Qubit budget is translated into an absolute budget B by using stock prices after filtering.
+    # User can also hard-code the budget here, allows for more capability.
+    B = 2**((np.sum(np.array([np.log(price)/np.log(2) for price in P])) + N)/filtering_number)
+    n_max = [np.floor(B/P[i]) for i in range(L)]
+    D = [np.ceil(np.log(n_max[i])/np.log(2)) for i in range(L)]
+    N = int(np.sum(np.array(D)))
+
+    # Co-Variance Matrix:
+    Sigma = [
+        [   np.sum(np.array([
+            (r[i][t]-mu[i])*(r[j][t]-mu[j]) for t in range( min( len(r[i]),  len(r[j])) )
+        ]))/(len(r[i]) - 1)
+            for j in range(L)
+        ] for i in range(L)
+    ]
+
+    # To better optimize the process, we include the budget 
+    # constraint and the fact that only
+    # an integer multiple of each stock can be bought. 
+    # This is to avoid unspent budget/overspending.
+
+    P_p = [P[i]/B for i in range(L)]
+    mu_p = [mu[i]*P_p[i] for i in range(L)]
+    Sigma_p = [[P_p[i]*P_p[j]*Sigma[i][j] for j in range(L)] for i in range(L)]
+
+    # Convert to Binary transformation.
+    C = [[] for i in range(L)]
+    for i in range(L):
+        for j in range(i):
+            C[i] += [0]*int(D[j])
+        C[i] += [2**d for d in range(int(D[i]))]
+        for j in range(i+1, L):
+            C[i] += [0]*int(D[j])
+    C = np.array(C)
+
+    mu_pp = C.T @ mu_p
+    Sigma_pp = C.T @ Sigma_p @ C
+    P_pp = C.T @ P_p
+
+    
+    q = 1 # User-Defined Risk aversion factor.
+    lamb = 10 # Budget penalty param.
+
+    # QUBO is mapped to an Ising Hamiltonian.
+    # Our idea leverages the quantum computer's inherent advantage of simulating
+    # physical systems by mapping optimization problem to a physical Hamiltonian.
+    # In this analogy, co-variance matrix elements are spin interaction factors
+    # and expected returns are external magnetic field.
+    Sigma_pp = np.array(Sigma_pp)
+    J = q/4 * Sigma_pp
+    h = [-0.5*mu_pp[i] + 0.5*q*(Sigma_pp@np.array([1 for j in range(N)]))[i] for i in range(N)]
+    pi = [P_pp[i]/2 for i in range(N)]
+    beta = 1 - np.sum(np.array([P_pp[i]/2 for i in range(N)]))
+
+
+    # We start the quantum protocol now. We'll apply successive 
+    # eigenstate filtering through QETU sequence. We start with a 
+    # random initial state.
+    vec = random_statevector(2**N)
+
+    spectrum_upper_bound = 1e2 # Can be put arbitrarily high but then the number of
+                               # successive filtering stages (M) should be increased
+                               # in exchange.
+    M = 3 # The more filtering stages you apply, the more  
+          # you are certain of achieving a lower energy subspace.
+
+    ket_0 = np.array([1, 0]) # ancilla qubit is init. as ket{0}
+    qcs_qetu = []
+    for i in range(M):
+
+        # This linear transformation compresses the spectrum into [0, pi].
+        spectrum_upper_bound = spectrum_upper_bound/2
+        spectrum_lower_bound = -1
+        max_spectrum_length = spectrum_upper_bound - spectrum_lower_bound
+        c1 = (np.pi) / (max_spectrum_length)
+        c2 = - c1 * spectrum_lower_bound
+    
+        # Time evolution block:
+        t = c1
+        qc_U = qiskit.QuantumCircuit(N)
+        qc_U_dag = qiskit.QuantumCircuit(N)
+        nsteps = 1
+        for n in range(nsteps):
+            qc_U.append(trotterized_time_evolution(J, h, pi, lamb, beta, t/nsteps, N).to_gate(), [i for i in range(N)])
+            qc_U_dag.append(trotterized_time_evolution(J, h, pi, lamb, beta, -t/nsteps, N).to_gate(), [i for i in range(N)])
+        
+        qc_cU = qiskit.QuantumCircuit(N+1)
+        qc_cU.append(qc_U.to_gate().control(), [N] + [i for i in range(N)])
+        qc_cU_dag = qiskit.QuantumCircuit(N+1)
+        qc_cU_dag.append(qc_U_dag.to_gate().control(), [N] + [i for i in range(N)])
+        qc_cfUs = [qc_cU, qc_cU_dag]
+        
+        # QETU Circuit:
+        mu, d, c, phis_max_iter, = (0.99, 30, 0.95, 10)
+        qc_qetu, _ = qetu_rqc_oneLayer(N, 0, 0, 1, mu, d=d, c2=c2,
+                                        qc_cU_custom=(qc_cfUs[0], qc_cfUs[1])
+                                        )
+        qcs_qetu.append(qc_qetu)
+
+    backend = Aer.get_backend("statevector_simulator") # Just for the sake of simulations we use
+                                                       # we use state_vector. This severely limits the
+                                                       # qubit budget in our simulations. 
+                                                       # If we had more time we could have implemented 
+                                                       # a more efficient version such that we could 
+                                                       # simulate with more qubits then circa 10 :)
+    qc_RQC = qiskit.QuantumCircuit(N+1, N+1)
+    qc_RQC.initialize(np.kron(ket_0, vec))
+    for qc_qetu in qcs_qetu:
+        # We apply QETU circuits one by one. In between each QETU sequence, we need to measure
+        # the ancilla qubit and make sure its measured as 0. If it's measured as 1 the sequence is
+        # canceled and we start over. Measuring the ancilla this way as 0 is probabilistic. 
+        # This code implementation reports the success probability of measuring the ancilla as 0
+        # at each filtering stage.
+        qc_RQC.append(qc_qetu.to_gate(), [i for i in range(N+1)])
+        bR = execute(transpile(qc_RQC), backend).result().get_statevector().data
+        aR = np.kron(np.array([[1,0],[0,0]]), np.identity(2**N)) @ bR
+        print("Projecting the ancilla qubit onto 0 with success prob: ", np.linalg.norm(aR)**2)
+        aR = aR / np.linalg.norm(aR)
+        qc_RQC.reset([i for i in range(N+1)])
+        qc_RQC.initialize(aR)
+
+    # The outcome we get as measurement result, converges more and more into a bunch of 
+    # bitstrings in the low energy manifold of our Hamiltonian. Even though we won't necessarily
+    # get ideal lowest energy state, we are guaranteed to be in the low energy sub-space thanks
+    # to the non-variational nature of our purely quantum algorithm :) 
+    # This is the advantage of non variational ground state preparation!
+
+    qc_to_measure = qiskit.QuantumCircuit(N+1, N+1)
+    qc_to_measure.initialize(aR)
+    qc_to_measure.measure([i for i in range(N)], [i for i in range(N)])
+    backend = Aer.get_backend("aer_simulator")
+    shots = 1e3
+    counts = execute(transpile(qc_to_measure), backend, shots=shots).result().get_counts()
+
+    opt_bstr = ''.join('1' if x == '0' else '0' for x in sorted(counts, key=counts.get, reverse=True)[2][1:])
+    b_Ising = []
+    for ch in opt_bstr:
+        b_Ising.append(0 if ch == '1' else 1)
+    b_Ising = np.array(b_Ising)
+    q_opt = C@b_Ising
+    print("Optimal Portfolio: ", q_opt)
+
+    # Turn the integer valued optimal stocks to weights.
+    total_money_to_invest = 0
+    result = {}
+    for i, index in enumerate(indices[-filtering_number:]):
+        total_money_to_invest+=q_opt[i]*P[i]
+        result[asset_names[index]] = q_opt[i]*P[i]
+    for i, index in enumerate(indices[-filtering_number:]):
+        result[asset_names[index]] = result[asset_names[index]]/total_money_to_invest
+
     return {'selected_assets_weights':result, 'num_selected_assets': len(result)}
+
+
+
+
+
+def trotterized_time_evolution(J, h, pi, lamb, beta, t, L):
+    """
+        Trotterized Time evolution encoding of the Ising Hamiltonian.
+    """
+    qc = qiskit.QuantumCircuit(L)
+    Z = np.array([[1.,  0.], [0., -1.]])
+
+    gates = []
+    for i in range(L):
+        for j in range(i+1, L):
+            hloc = (J[i][j] + J[j][i] + 2*lamb*pi[i]*pi[j]) * np.kron(Z, Z)
+            qc2 = qiskit.QuantumCircuit(2)
+            qc2.unitary(scipy.linalg.expm(-1j*t*0.5*hloc), [0, 1], label='str')
+            gates.append((qc2, [L-i-1, L-j-1]))
+            qc.append(gates[-1][0].to_gate(), gates[-1][1])
+    gates.reverse()
+    for gate in gates:
+        qc.append(gate[0].to_gate(), gate[1])
+
+    for i in range(L):
+        qc.rz(2*t*(h[i] - 2*beta*lamb*pi[i]), L-i-1)
+
+    qc.p(
+        -t*(lamb*beta**2+np.sum(np.array([J[i][i] + lamb*pi[i]*pi[i] for i in range(L)]))), L-1
+    )
+    qc.x(L-1)
+    qc.p(
+        -t*(lamb*beta**2+np.sum(np.array([J[i][i] + lamb*pi[i]*pi[i] for i in range(L)]))), L-1
+    )
+    qc.x(L-1)
+    return qc
--- src/groundstate_prep/ground_state_prep_qiskit.py
+++ src/groundstate_prep/ground_state_prep_qiskit.py
@@ -1,6 +1,5 @@
 import qiskit
 import scipy
-import h5py
 import os
 import numpy as np
 from pyqsp.completion import CompletionError
@@ -14,266 +13,27 @@ from qiskit.providers.aer.noise import NoiseModel, errors, coherent_unitary_erro
 import rqcopt as oc
 import qiskit.quantum_info as qi
 
-from ground_state_prep import get_phis
-from utils_gsp import construct_ising_local_term, approx_polynomial, get_phis, qc_U, qc_U_Strang, qc_QETU_cf_R, qc_QETU_R, QETU, QETU_heis_cf
+from utils_gsp import construct_ising_local_term, approx_polynomial, get_phis, qc_U, qc_U_Strang, qc_QETU_cf_R, qc_QETU_R, get_phis
 
 import sys
 sys.path.append("../../src/rqcopt")
 from optimize import ising1d_dynamics_opt
 
-decomp = TwoQubitBasisDecomposer(gate=CXGate())
-
-# We demonstrate that we can implement the QETU Circuit with the TFIM Hamiltonian
-# without including a controlled time evolution operator. This can be achieved by inserting controlled-
-# single qubit gates, before and after the time evolution gate. To implement the conjugated time evolution
-# operator, we insert X gates to the ancilla qubit, before and after the controlled-time evolution gate.
 
 
-def get_error_from_sv(qc, qc_H, depolarizing_error, reps, L, J, g, ev, nshots=1e5, t1=3e10, 
-    t2=3e10, gate_t=100, coherent_error=0, mid_cbits=0):
-    backend = Aer.get_backend("aer_simulator")
-    backend._configuration.max_shots = 1e10
-    
-    x1_error = errors.depolarizing_error(depolarizing_error*0.1, 1)
-    x2_error = errors.depolarizing_error(depolarizing_error, 2)
-    relax_error = errors.thermal_relaxation_error(t1, t2, gate_t)
-    x1_error = x1_error.compose(relax_error)
-    x2_error = x2_error.compose(relax_error)
-    no_error = errors.depolarizing_error(0, L+1)
-    noise_model = NoiseModel()
-    noise_model.add_all_qubit_quantum_error(x1_error, ['u1', 'u2', 'u3'])
-    noise_model.add_all_qubit_quantum_error(x2_error, ['cx', 'cy', 'cz', 'unitary'])
-    noise_model.add_all_qubit_quantum_error(no_error, ['meas0'])
-    noise_model.add_basis_gates(['unitary', 'meas0'])
-
-    # Coherent overrotation around a random axis error.
-    epsilon = coherent_error
-    pauli = [np.array([[0, 1], [1, 0]]), np.array([[0, -1j], [1j, 0]]), np.array([[1, 0], [0, -1]])]
-    n = np.random.rand(3)
-    n = n / np.linalg.norm(n)
-    rot = np.zeros(2)
-    for i, paul in enumerate(pauli):
-        rot = rot + paul*n[i] 
-
-    u_err_h =  scipy.linalg.expm(1j*epsilon*rot)
-    noise_model.add_all_qubit_quantum_error(coherent_unitary_error(u_err_h), 'h')
-    
-    E_nps = []
-    for i in range(reps):
-        print("getting counts")
-        counts_dict1_all = execute(transpile(qc), backend, noise_model=noise_model, basis_gates=noise_model.basis_gates, shots=nshots).result().get_counts()
-        counts_dict2_all = execute(transpile(qc_H), backend, noise_model=noise_model, basis_gates=noise_model.basis_gates, shots=nshots).result().get_counts()
-        print("gotten counts")
-
-
-        if mid_cbits == 0:
-            counts_dict1 = counts_dict1_all
-            counts_dict2 = counts_dict2_all
-        else:
-            counts_dict1 = {}
-            for key in counts_dict1_all.keys():
-                if key[-mid_cbits:] == '0'*mid_cbits:
-                    counts_dict1[key[:L+1]] = counts_dict1_all[key]
-
-            counts_dict2 = {}
-            for key in counts_dict2_all.keys():
-                if key[-mid_cbits:] == '0'*mid_cbits:
-                    counts_dict2[key[:L+1]] = counts_dict2_all[key]
-
-        
-        E_np = estimate_eigenvalue_counts(counts_dict1, counts_dict2, L, J, g)
-        E_nps.append(E_np)
-        print(E_np)
-    E_nps = np.array(E_nps)
-
-    return np.linalg.norm(np.sum(E_nps)/E_nps.size - ev)
-
-
-def prepare_ground_state_qiskit(L, J, g, t, mu, a_values, c2=0, d=30, c=0.95, steep=0.01, 
-        max_iter_for_phis=10, repeat_qetu=3, RQC_layers=3, init_state=None, ground_state=None, reuse_RQC=False, eps=0):
-    V_list = []
-    dirname = os.path.dirname(os.path.realpath(__file__))
-    path = os.path.join(dirname, f"../../src/rqcopt/results/ising1d_L{reuse_RQC if reuse_RQC else L}_t{t}_dynamics_opt_layers{RQC_layers}.hdf5")
-
-    try:
-        with h5py.File(path, "r") as f:
-            #assert f.attrs["L"] == L
-            assert f.attrs["J"] == J
-            assert f.attrs["g"] == g
-            assert f.attrs["t"] == t
-            V_list = list(f["Vlist"])
-    except FileNotFoundError:
-        strang = oc.SplittingMethod.suzuki(2, 1)
-        _, coeffs_start_n5 = oc.merge_layers(2*strang.indices, 2*strang.coeffs)
-        # divide by 2 since we are taking two steps
-        coeffs_start_n5 = [0.5*c for c in coeffs_start_n5]
-        print("coeffs_start_n5:", coeffs_start_n5)
-        V_list = ising1d_dynamics_opt(5, t, False, coeffs_start_n5, path, niter=16)
-
-
-    perms = [None if i % 2 == 0 else np.roll(range(L), -1) for i in range(len(V_list))]
-
-    print("Running decomposition of two-qubit gates of the RQC Circuit...")
-    qcs_rqc = []
-    for V in V_list:
-        #decomp = TwoQubitBasisDecomposer(gate=CXGate())
-        #qc_rqc = decomp(V)
-        qc_rqc = qiskit.QuantumCircuit(2)
-        qc_rqc.unitary(V, [0, 1])
-        qcs_rqc.append(qc_rqc)
-
-    backend = Aer.get_backend("statevector_simulator")    
-
-    phis = []
-    i = 0
-    while True:
-        try:
-            phis = get_phis(mu, d, steep, c=c)
-            break
-        except CompletionError:
-            print("Completion Error encountered!")
-            if i>max_iter_for_phis:
-                raise Exception("Max Iteration for estimating the phis breached!")
-            i = i + 1
-            c = c - 0.01
-            print(f"c updated to {c}!")
-            if i > max_iter_for_phis / 2:
-                print(f"QSP did not work for d = {d}, updating d to {d-4}")
-                d = d - 4
-        except AngleFindingError:
-            print("AngleFindingError encountered!")
-            if i>max_iter_for_phis:
-                raise Exception("Max Iteration for estimating the phis breached!")
-            i = i + 1
-            
-            if i > max_iter_for_phis / 2:
-                print(f"QSP did not work for d = {d}, updating d to {d-4}")
-                d = d - 4
-    phis_list = [phi+eps for phi in phis[0]]
-    
-    print("F(a_max)^2: ", phis[2](a_values[0])**2)
-
-    qc_U_ins = qc_U(qcs_rqc, L, perms)
-    qc_QETU = qc_QETU_cf_R(qc_U_ins, phis_list, c2)
-    qc_ins = qiskit.QuantumCircuit(L+1, L+1)
-
-    statevectors_bR = []
-    statevectors_aR = []
-
-    if init_state is not None:
-        qc_ins.initialize(init_state)
-    for i in range(repeat_qetu):
-        print("Layer ", i)
-        qc_ins.append(qc_QETU.to_gate(), [i for i in range(L+1)])
-        bR = execute(transpile(qc_ins), backend).result().get_statevector().data
-        statevectors_bR.append(bR)
-        # TODO: This workaround should be only temporary! Reset is not working as expected!
-        #qc_ins.reset(L)
-        #aR = execute(transpile(qc_ins), backend).result().get_statevector()
-        aR = np.kron(np.array([[1,0],[0,0]]), np.identity(2**L)) @ bR
-        aR = aR / np.linalg.norm(aR)
-        statevectors_aR.append(aR)
-        statePrep_Gate = StatePreparation(aR, label='meas0')
-        qc_ins.reset([i for i in range(L+1)])
-        qc_ins.append(statePrep_Gate, [i for i in range(L+1)])
-
-    qc_ins2 = qc_ins.copy()
-    qc_ins2.h([i for i in range(L)])
-    qc_ins.measure([i for i in range(L+1)], [i for i in range(L+1)])
-    qc_ins2.measure([i for i in range(L+1)], [i for i in range(L+1)])
-
-    qc_RQC = qc_ins.copy()
-    qc_H_RQC = qc_ins2.copy()
-
-    backend = Aer.get_backend("statevector_simulator")
-    qc_U_ins = qc_U_Strang(L, J, g, t, 1)
-    qc_QETU = qc_QETU_cf_R(qc_U_ins, phis_list, c2)
-    qc_ins = qiskit.QuantumCircuit(L+1, L+1)
-
-    if init_state is not None:
-        qc_ins.initialize(init_state)
-    for i in range(repeat_qetu):
-        print("Layer ", i)
-        qc_ins.append(qc_QETU.to_gate(), [i for i in range(L+1)])
-        bR = execute(transpile(qc_ins), backend).result().get_statevector().data
-        statevectors_bR.append(bR)
-        aR = np.kron(np.array([[1,0],[0,0]]), np.identity(2**L)) @ bR
-        aR = aR / np.linalg.norm(aR)
-        statevectors_aR.append(aR)
-        statePrep_Gate = StatePreparation(aR, label='meas0')
-        qc_ins.reset([i for i in range(L+1)])
-        qc_ins.append(statePrep_Gate, [i for i in range(L+1)])
-        #qc_ins.reset(L)
-
-    qc_ins2 = qc_ins.copy()
-    qc_ins2.h([i for i in range(L)])
-    qc_ins.measure([i for i in range(L+1)], [i for i in range(L+1)])
-    qc_ins2.measure([i for i in range(L+1)], [i for i in range(L+1)])
-
-    qc_STR = qc_ins.copy()
-    qc_H_STR = qc_ins2.copy()
-    return qc_RQC, qc_H_RQC, qc_STR, qc_H_STR
-
-
-
-def Pr_counts(counts, j, zj, *zjplus1):
-    # Helper Function to retrieve the probability of measuring the qubit number j (and optionally j+1)
-    prob = 0
-    nshots = 0
-    for key, value in counts.items():
-        nshots += value
-    
-    for bitstring, count in counts.items():
-        bitstring = bitstring[::-1]
-        bitstring = bitstring[:-1]
-        
-        L = len(bitstring)
-        
-        if not zjplus1:
-            if bitstring[j] == str(zj):
-                prob += count/nshots
-        else:
-            if j == L-1:
-                if bitstring[j] == str(zj) and bitstring[0] == str(zjplus1[0]):
-                    prob += count/nshots
-            else:
-                if bitstring[j] == str(zj) and bitstring[j+1] == str(zjplus1[0]):
-                    prob += count/nshots
-    return prob
-
-
-def estimate_eigenvalue_counts(counts1, counts2, L, J, g):
-    E1 = 0
-    # For consistency with qib, upper range has to be L!
-    for j in range(L):
-        for zj in range(2):
-            for zjplus1 in range(2):
-                E1 = E1 + (((-1)**(zj + zjplus1)) * Pr_counts(counts1, j, zj, zjplus1))
-    
-    E2 = 0
-    for j in range(L):
-        for zj in range(2):
-            E2 = E2 + (((-1)**zj) * Pr_counts(counts2, j, zj))
-
-    return J*E1 + g*E2
-
-
-
-def qetu_rqc_oneLayer(L, J, g, t, mu, a_values, c2=0, d=30, c=0.95, 
+def qetu_rqc_oneLayer(L, J, g, t, mu, c2=0, d=30, c=0.95, 
     steep=0.01, max_iter_for_phis=10, RQC_layers=5, 
     init_state=None, split_U=1, reuse_RQC=0, qc_U_custom=None,
     custom_qc_QETU_cf_R=None, qc_cU_custom=None, hamil=None,
     H1=None, H2=None, heis_c2=0, eps=0, lambda_est=None, reverse=False,
     multi_ancilla=1, a_assess=None
     ):
-    # One QETU Layer for TFIM Hamiltonian. You can also custom give the time
-    # evolution block and the control-free implementation of QETU.
+    # Implements one QETU Layer.
 
     t = t/split_U
-    print("dt: ", t)
     V_list = []
     dirname = os.path.dirname(os.path.realpath(__file__))
-    path = os.path.join(dirname, f"../../../aff/src/rqcopt/results/ising1d_L{reuse_RQC if reuse_RQC else L}_t{t}_dynamics_opt_layers{RQC_layers}.hdf5")
+    #path = os.path.join(dirname, f"../../../aff/src/rqcopt/results/ising1d_L{reuse_RQC if reuse_RQC else L}_t{t}_dynamics_opt_layers{RQC_layers}.hdf5")
 
     if qc_U_custom is None and qc_cU_custom is None:	
         try:
@@ -302,11 +62,9 @@ def qetu_rqc_oneLayer(L, J, g, t, mu, a_values, c2=0, d=30, c=0.95,
 
     perms = [None if i % 2 == 0 else np.roll(range(L), -1) for i in range(len(V_list))]
 
-    print("Running decomposition of two-qubit gates of the RQC Circuit...")
+    print("Constructing QETU sequence")
     qcs_rqc = []
     for V in V_list:
-        #decomp = TwoQubitBasisDecomposer(gate=CXGate())
-        #qc_rqc = decomp(V)
         qc_rqc = qiskit.QuantumCircuit(2)
         qc_rqc.unitary(V, [0, 1])
         qcs_rqc.append(qc_rqc)
@@ -339,11 +97,6 @@ def qetu_rqc_oneLayer(L, J, g, t, mu, a_values, c2=0, d=30, c=0.95,
                 d = d - 4
     phis_list = [phi+eps for phi in phis[0]]
 
-    print("F(a_max)^2: ", phis[2](a_values[0])**2)
-    print("F(a_premax)^2: ", phis[2](a_values[1])**2)
-    print("F(x)^2: ", phis[2](mu)**2)
-    if a_assess is not None:
-        print("F(a_assess)^2: ", phis[2](a_values[a_assess])**2)
 
     qc_U_ins = qiskit.QuantumCircuit(L)
     if qc_U_custom is None and lambda_est is None:
@@ -368,24 +121,7 @@ def qetu_rqc_oneLayer(L, J, g, t, mu, a_values, c2=0, d=30, c=0.95,
         qc_QETU = qc_QETU_cf_R(qc_U_ins, phis_list, c2)
 
     qc_ins = qiskit.QuantumCircuit(L+multi_ancilla)
-
-    QETU_mat = None
-    if hamil is not None and H1 is None:
-        backend = Aer.get_backend("unitary_simulator")
-        qc_unit = execute(transpile(qc_QETU), backend).result().get_unitary(qc_QETU, L+multi_ancilla).data
-        QETU_mat = QETU(hamil, phis_list, t, c2)
-        print("QETU Error: ", np.linalg.norm(qc_unit-QETU_mat, ord=2))
-
-    if H1 is not None and H2 is not None:
-        backend = Aer.get_backend("unitary_simulator")
-        QETU_mat = QETU_heis_cf(H1, H2, phis_list, t, heis_c2, 10)
-        qc_unit = execute(transpile(qc_QETU), backend).result().get_unitary(qc_QETU, L+1).data
-        print("QETU Error: ", np.linalg.norm(qc_unit-QETU_mat, ord=2))
-
-
-    if init_state is not None:
-        qc_ins.initialize(init_state)
     qc_ins.append(qc_QETU.to_gate(), [i for i in range(L+multi_ancilla)])
 
 
-    return qc_ins, (phis_list, phis[1], phis[2]), QETU_mat
+    return qc_ins, (phis_list, phis[1], phis[2])
